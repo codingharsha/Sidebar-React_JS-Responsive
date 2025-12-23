2be3b638aca79f08b4061494cbea89ce8d6ec24c
codingharsha <harshavarthanoff@gmail.com>
2025-12-23 12:32:37 +0530

    Sidebar started, and changes need to be done on Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { CgProfile } from "react-icons/cg";
-import { IoSearch } from "react-icons/io5";
+import { IoSearchSharp } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { IoLogoGameControllerB } from "react-icons/io";
 
 const Navbar = () => {
+
+    const[isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    }
+
   return (
     <div className='navbar'>
         <div className="left-section">
-            <IoLogoGameControllerB className='logo'/>
-            <p>Harsha <br />Games</p>
+            <GiHamburgerMenu className='hamburger-icon'/>
+            <div className="logo-container">
+                <IoLogoGameControllerB className='logo'/>
+                <p>Harsha <br />Games</p>
+            </div>
         </div>
-        <div className="mid-section">
+
+        <div className="right-section">
             <ul>
                 <li>Home</li>
                 <li>Play Online</li>
                 <li>Downloads</li>
             </ul>
-        </div>
-        <div className="right-section">
             <div className="search-box">
-                <IoSearch className='search-icon'/>
+                <IoSearchSharp className='search-icon'/>
                 <input type="text" placeholder='Search...' />
             </div>
             <CgProfile className='profile-icon'/>
